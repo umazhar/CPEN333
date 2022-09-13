2a1f6fe69c8a1da1608ac7ef4dfb7d0bb8032b8a
Umair Mazhare <u.mazhar@icloud.com>
2022-09-12 21:17:29 -0700

    Started lab1

---
+++ lab1/Lab1.py
@@ -0,0 +1,58 @@
+# student name: Umair Mazhar
+# student number: 20333308
+
+# A command-line Tic-Tac-Toe game 
+import random
+
+board = [' '] * 9 # A list of 9 strings, one for each cell, 
+                  # will contain ' ' or 'X' or 'O'
+played = set()    # A set to keep track of the played cells 
+
+def init() -> None:
+    """ prints the banner messages 
+        and prints the intial board on the screen
+    """
+    print("Welcome to Tic-Tac-Toe!")
+    print("You play X (first move) and computer plays O.")
+    print("Computer plays randomly, not strategically.")
+    printBoard()
+
+def printBoard() -> None:
+    """ prints the board on the screen based on the values in the board list """
+    pass #To Implement
+
+
+def playerNextMove() -> None:
+    """ prompts the player for a valid cell number, 
+        and prints the info and the updated board;
+        error checks that the input is a valid cell number 
+    """
+    pass #To Implement
+
+def computerNextMove() -> None:
+    """ Computer randomly chooses a valid cell, 
+        and prints the info and the updated board 
+    """
+    pass #To Implement
+
+def hasWon(who: str) -> bool:
+    """ returns True if who (being passed 'X' or 'O') has won, False otherwise """
+    pass #To Implemen
+
+def terminate(who: str) -> bool:
+    """ returns True if who (being passed 'X' or 'O') has won or if it's a draw, False otherwise;
+        it also prints the final messages:
+                "You won! Thanks for playing." or 
+                "You lost! Thanks for playing." or 
+                "A draw! Thanks for playing."  
+    """
+    pass #To Implement
+
+if __name__ == "__main__":
+    # Use as is. 
+    init()
+    while True:
+        playerNextMove()            # X starts first
+        if(terminate('X')): break   # if X won or a draw, print message and terminate
+        computerNextMove()          # computer plays O
+        if(terminate('O')): break   # if O won or a draw, print message and terminate
